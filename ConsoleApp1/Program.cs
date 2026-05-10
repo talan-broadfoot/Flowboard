f3779ea8c144582d5a6d19438d3a85d83fc2a510
talan-broadfoot <talanbroadfoot@gmail.com>
2026-05-10 17:01:09 -0500

    Created class and object files
    Created a class blueprint in project.cs and used that class in program.cs to create an object, set properties, and assign values.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,9 @@
-﻿Console.WriteLine("Wecome to Flowboard!");
+﻿using ConsoleApp1;
+
+Project flowboard = new Project();
+flowboard.Name = "Flowboard";
+flowboard.TaskCount = 0;
+flowboard.IsComplete = false;
+Console.WriteLine($"Project: {flowboard.Name}");
+Console.WriteLine($"Tasks: {flowboard.TaskCount}");
+Console.WriteLine($"Complete: {flowboard.IsComplete}");
---
+++ ConsoleApp1/Project.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Project
+    {
+        public string Name;
+        public int TaskCount;
+        public bool IsComplete;
+    }
+}

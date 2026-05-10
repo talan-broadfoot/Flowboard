237b7e220f1ed0a880dfa51d48ba3ac4a337e7f9
talan-broadfoot <talanbroadfoot@gmail.com>
2026-05-10 17:25:43 -0500

    Methods and types
    Learned the difference between void and  return types also added method for project details.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,6 +4,11 @@ Project flowboard = new Project();
 flowboard.Name = "Flowboard";
 flowboard.TaskCount = 0;
 flowboard.IsComplete = false;
-Console.WriteLine($"Project: {flowboard.Name}");
-Console.WriteLine($"Tasks: {flowboard.TaskCount}");
-Console.WriteLine($"Complete: {flowboard.IsComplete}");
+
+void PrintProjectDetails(Project project)
+{
+    Console.WriteLine($"Project: {project.Name}");
+    Console.WriteLine($"Tasks: {project.TaskCount}");
+    Console.WriteLine($"Complete: {project.IsComplete}");
+}
+PrintProjectDetails(flowboard);

e0c43c6bde4d3db17e3e2fc98893e359a5bbc4e9
talan-broadfoot <talanbroadfoot@gmail.com>
2026-05-22 12:50:41 -0500

    got project form working with ngModel and addProject method

--- web/flowboard-web/src/app/projects/projects.ts
+++ web/flowboard-web/src/app/projects/projects.ts
@@ -1,14 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from '../project.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './projects.html',
   styleUrl: './projects.css',
 })
 export class Projects implements OnInit {
   projects: any[] = [];
+  newProjectName: string = '';
+  newProjectStatus: string = '';
 
   constructor(private projectService: ProjectService) {}
 
@@ -16,6 +19,12 @@ export class Projects implements OnInit {
     this.projectService.getProjects().subscribe(data => {
       console.log(data);
       this.projects = data;
+    })
+  }
+  addProject() {
+    this.projectService.createProject(this.newProjectName, this.newProjectStatus).subscribe(newProject => {
+      console.log(newProject)
+      this.projects.push(newProject);
     })
   }
 }

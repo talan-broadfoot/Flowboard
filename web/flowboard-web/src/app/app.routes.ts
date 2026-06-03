02b44d7ef15d595dca0adf6bb48b398f445c666a
talan-broadfoot <talanbroadfoot@gmail.com>
2026-06-03 11:46:07 -0500

    Added default route to redirect to /projects

--- web/flowboard-web/src/app/app.routes.ts
+++ web/flowboard-web/src/app/app.routes.ts
@@ -4,5 +4,6 @@ import { ProjectDetail } from './project-detail/project-detail';
 
 export const routes: Routes = [
   { path: 'projects', component: Projects },
-  { path: 'projects/:id', component: ProjectDetail }
+  { path: 'projects/:id', component: ProjectDetail },
+  { path: "", redirectTo: '/projects', pathMatch: 'full' }
 ];

0c170c7ba15481e3d18b4b64b8ccc2f2409d50a4
talan-broadfoot <talanbroadfoot@gmail.com>
2026-06-18 18:21:22 -0500

    Added auth services: login, register, logout, getToken, and isLoggedIn

--- web/flowboard-web/src/app/auth.ts
+++ web/flowboard-web/src/app/auth.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http'
+import { tap } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class Auth {
   constructor(private http: HttpClient) { }
+  login(username: string, password: string) {
+    return this.http.post<{ token: string }>('https://localhost:7045/api/auth/login',{username, password})
+    .pipe(tap(response => localStorage.setItem('token', response.token)));
+  }
+  getToken() {
+    return localStorage.getItem('token');
+  }
+  isLoggedIn() {
+    return this.getToken() !== null;
+  }
+  logout() {
+    localStorage.removeItem('token');
+  }
+  register(username: string, password: string) {
+    return this.http.post('https://localhost:7045/api/auth/register',{username, password});
+  }
 }

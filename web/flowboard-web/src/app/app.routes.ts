672777b4f9b3bba07502500bc33af960f6b6be71
talan-broadfoot <talanbroadfoot@gmail.com>
2026-06-19 14:39:36 -0500

    Add auth route and protect projects' routes

--- web/flowboard-web/src/app/app.routes.ts
+++ web/flowboard-web/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Projects } from './projects/projects';
 import { ProjectDetail } from './project-detail/project-detail';
+import { authGuard } from './auth-guard'
 
 export const routes: Routes = [
-  { path: 'projects', component: Projects },
-  { path: 'projects/:id', component: ProjectDetail },
+  { path: 'projects', component: Projects, canActivate: [authGuard] },
+  { path: 'projects/:id', component: ProjectDetail, canActivate: [authGuard] },
   { path: "", redirectTo: '/projects', pathMatch: 'full' }
 ];
---
+++ web/flowboard-web/src/app/auth-guard.ts
@@ -0,0 +1,13 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from '@angular/core'
+import { Auth } from './auth'
+
+export const authGuard: CanActivateFn = (route, state) => {
+  const auth = inject(Auth);
+  const router = inject(Router);
+  if(auth.isLoggedIn()) {
+    return true;
+  }
+  router.navigate(['/login']);
+  return false;
+};
